c88243419ef7c6534785002e11e46c6d5a326f20
hil679 <hil679922@gmail.com>
2024-04-08 23:43:01 +0900

    transaction: 트렌젝션 적용1

---
+++ src/main/java/com/spring/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package com.spring.jdbc.service;
+
+import com.spring.jdbc.domain.Member;
+import com.spring.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepositoryV1;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException { //원래 예외 잘 던지지 않, 지금은 간편히 던짐
+        Member fromMember = memberRepositoryV1.findById(fromId);
+        Member toMember = memberRepositoryV1.findById(toId);
+
+        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+    }
+
+    /* option + command + m -> 함수로 extract*/
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
