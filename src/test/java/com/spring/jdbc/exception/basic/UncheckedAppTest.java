427160f1392e186a19fc658963cc19ccb1dff664
hil679 <hil679922@gmail.com>
2024-04-14 23:04:35 +0900

    section5: 예외 포함과 스택 트래이스

--- src/test/java/com/spring/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/com/spring/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,5 +1,6 @@
 package com.spring.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
@@ -7,6 +8,7 @@ import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UncheckedAppTest {
     @Test
     void checked() {
@@ -14,6 +16,18 @@ public class UncheckedAppTest {
         assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(RuntimeSqlException .class); // Exception.class 여도 test 성공
     }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace(); // 할 수는 있지만 좋지 않은 방법!!!!!
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
         Service service = new Service();
 
@@ -41,6 +55,7 @@ public class UncheckedAppTest {
             try {
                 runSql();
             } catch (SQLException e) {
+                // throw new RuntimeSqlException(); // 기 존 예외 trace 안 나옴, 안 넘겨줬기 때문
                 throw new RuntimeSqlException(e); //예외를 던질 땐 기존 예외를 던져줘야 기존 트레이스까지 확인 가능
             }
         }
@@ -58,6 +73,9 @@ public class UncheckedAppTest {
     }
 
     static class RuntimeSqlException extends RuntimeException {
+        public RuntimeSqlException() {
+        }
+
         public RuntimeSqlException(Throwable cause) { //cause -> 왜 발생했는지 이전 예외를 같이 넣을 수 있다.
             super(cause);
         }
